670de0513c072e350ef1b763daa18d4d5cc3aad0
Luciano Rodrigo Ferretto <prof.ferretto@yahoo.com.br>
2023-08-30 22:05:44 -0300

    Final da aula

--- Aula06_OO/FeitoEmAula/CadVeiculo.java
+++ Aula06_OO/FeitoEmAula/CadVeiculo.java
@@ -5,17 +5,22 @@ public class CadVeiculo {
         num2++;
         System.out.println("Num1 = " + num1 + " - Num2 = " + num2);
         //Solicitei para o usuario e ele me passou os dados do veiculo
-        Veiculo meuVeiculo = new Veiculo();
-        Veiculo teuVeiculo = new Veiculo();
-        meuVeiculo.marca = "Fiat";
-        meuVeiculo.modelo = "Uno de Firma com escada";
-        meuVeiculo.ano = 2005;
+        Veiculo meuVeiculo = new Veiculo("Fiat", "Uno", 2005);
+        Veiculo teuVeiculo = new Veiculo(2020);
+        //meuVeiculo.marca = "Fiat";
+        //meuVeiculo.modelo = "Uno de Firma com escada";
+        //meuVeiculo.ano = 2005;
         meuVeiculo.ligar();
         teuVeiculo.marca = "Volkswagem";
         teuVeiculo.modelo = "Fusca do Itamar";
         teuVeiculo.ano = 1996;
         teuVeiculo.ligar();
 
+        Veiculo cloneMeuVeiculo = meuVeiculo.clonar();
+
+        // Retorna a referência a um objeto do tipo Veiculo
+        Veiculo cloneTeuVeiculo = teuVeiculo.clonarErrado();
+        // Atribuição por Referência
         Veiculo outroVeiculo = teuVeiculo;
         outroVeiculo.ano = 1994;
 
@@ -24,6 +29,10 @@ public class CadVeiculo {
         int tempoMeuVeiculo = meuVeiculo.calculaTempoUso();
         int tempoTeuVeiculo = teuVeiculo.calculaTempoUso();
         int tempoOutroVeiculo = outroVeiculo.calculaTempoUso();
+
+        int tempoMeuVeiculo2030 = meuVeiculo.calculaTempoUso(2030);
+        int tempoTeuVeiculo2030 = teuVeiculo.calculaTempoUso(2030);
+        int tempoOutroVeiculo2030 = outroVeiculo.calculaTempoUso(2030);
 
         System.out.println("Marca: " + meuVeiculo.marca +
                             " - Modelo: " + meuVeiculo.modelo +
--- Aula06_OO/FeitoEmAula/Veiculo.java
+++ Aula06_OO/FeitoEmAula/Veiculo.java
@@ -1,12 +1,55 @@
+import java.time.LocalDate;
+
 public class Veiculo {
     String marca;
     String modelo;
     int ano;
+
+    Veiculo() {
+        this("MARCA NÃO DEFINIDA");
+    }
+    
+    Veiculo( String marca) {
+        this(marca, "MODELO NÃO DEFINIDO");
+    }
+    Veiculo( String marca, String modelo) {
+        this(marca, modelo, -1);
+    }
+    Veiculo(int ano) {
+        //this("MARCA","MODELO",ano);
+        this.ano = ano;
+    }
+    public Veiculo(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+
+
+    Veiculo clonar() {
+        Veiculo veiculo = new Veiculo();
+        veiculo.marca = this.marca;
+        veiculo.modelo = this.modelo;
+        veiculo.ano = this.ano;
+        return veiculo;
+    }
+
+    Veiculo clonarErrado() {
+        return this;
+    }
+
     int calculaTempoUso() {
-        int tempo = 2023 - this.ano;
+        int anoBase  = LocalDate.now().getYear();
+        int tempo = this.calculaTempoUso(anoBase);
+        return tempo;
+    }
+    int calculaTempoUso(int anoBase) {
+        int tempo = anoBase - this.ano;
         return tempo;
         //System.out.println("Tempo de uso: " + tempo + " ano(s)");
     }
+
     void ligar() {
         System.out.println("Veículo " + this.modelo + " ligado!");
     }
